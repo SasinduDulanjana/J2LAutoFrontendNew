b4d2216491514a6a4d8629a98c01b22f80c6c8bf
sasindu.paththamperuma@gmail.com <sasindu.paththamperuma@gmail.com>
2025-10-07 11:58:42 +0530

    changed backend url

--- src/app/base-url.ts
+++ src/app/base-url.ts
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'https://trueapp-d8a6bdaygsefajhg.canadacentral-01.azurewebsites.net';
+// export const BASE_URL = 'https://trueapp-d8a6bdaygsefajhg.canadacentral-01.azurewebsites.net';
+export const BASE_URL = 'https://smartposspartparts-1027202496567.asia-southeast1.run.app';
